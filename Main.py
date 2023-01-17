98270da55524311c03557db6c9b4eb64f9375507
Pouya <popibani1379@gmail.com>
2023-01-17 19:50:26 +0330

    UI Changes

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from tkinter import *
 from tkinter import filedialog
+from tkinter.font import Font
 import pytube.exceptions
 from pytube import YouTube
 
@@ -38,6 +39,8 @@ def youtube_screen():
     bg_label = Label(image=bg)
     bg_label.place(x=-6, y=-2)
 
+    default_font = Font(family="MV Boli", size=15)
+
     def res_getter(resolution):
         global resolution_value
         global button_144p
@@ -52,35 +55,35 @@ def youtube_screen():
             button_144p = PhotoImage(file=resource_path("selected_144p_button.png"))
             button_144p_label = Label(image=button_144p, borderwidth=0)
             button_144p_label.place(x=30, y=385)
-            real_button_144p = Button(root, image=button_144p, borderwidth=0, activebackground="#323232", bg="#323232",
+            real_button_144p = Button(root, image=button_144p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                       command=lambda: res_getter("144p"))
             real_button_144p.place(x=30, y=385)
 
             button_240p = PhotoImage(file=resource_path("240p_button.png"))
             button_240p_label = Label(image=button_240p, borderwidth=0)
             button_240p_label.place(x=110, y=385)
-            real_button_240p = Button(root, image=button_240p, borderwidth=0, activebackground="#323232", bg="#323232",
+            real_button_240p = Button(root, image=button_240p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                       command=lambda: res_getter("240p"))
             real_button_240p.place(x=110, y=385)
 
             button_360p = PhotoImage(file=resource_path("360p_button.png"))
             button_360p_label = Label(image=button_360p, borderwidth=0)
             button_360p_label.place(x=190, y=385)
-            real_button_360p = Button(root, image=button_360p, borderwidth=0, activebackground="#323232", bg="#323232",
+            real_button_360p = Button(root, image=button_360p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                       command=lambda: res_getter("360p"))
             real_button_360p.place(x=190, y=385)
 
             button_480p = PhotoImage(file=resource_path("480p_button.png"))
             button_480p_label = Label(image=button_480p, borderwidth=0)
             button_480p_label.place(x=270, y=385)
-            real_button_480p = Button(root, image=button_480p, borderwidth=0, activebackground="#323232", bg="#323232",
+            real_button_480p = Button(root, image=button_480p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                       command=lambda: res_getter("480p"))
             real_button_480p.place(x=270, y=385)
 
             button_720p = PhotoImage(file=resource_path("720p_button.png"))
             button_720p_label = Label(image=button_720p, borderwidth=0)
             button_720p_label.place(x=350, y=385)
-            real_button_720p = Button(root, image=button_720p, borderwidth=0, activebackground="#323232", bg="#323232",
+            real_button_720p = Button(root, image=button_720p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                       command=lambda: res_getter("720p"))
             real_button_720p.place(x=350, y=385)
 
@@ -88,35 +91,35 @@ def youtube_screen():
             button_144p = PhotoImage(file=resource_path("144p_button.png"))
             button_144p_label = Label(image=button_144p, borderwidth=0)
             button_144p_label.place(x=30, y=385)
-            real_button_144p = Button(root, image=button_144p, borderwidth=0, activebackground="#323232", bg="#323232",
+            real_button_144p = Button(root, image=button_144p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                       command=lambda: res_getter("144p"))
             real_button_144p.place(x=30, y=385)
 
             button_240p = PhotoImage(file=resource_path("selected_240p_button.png"))
             button_240p_label = Label(image=button_240p, borderwidth=0)
             button_240p_label.place(x=110, y=385)
-            real_button_240p = Button(root, image=button_240p, borderwidth=0, activebackground="#323232", bg="#323232",
+            real_button_240p = Button(root, image=button_240p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                       command=lambda: res_getter("240p"))
             real_button_240p.place(x=110, y=385)
 
             button_360p = PhotoImage(file=resource_path("360p_button.png"))
             button_360p_label = Label(image=button_360p, borderwidth=0)
             button_360p_label.place(x=190, y=385)
-            real_button_360p = Button(root, image=button_360p, borderwidth=0, activebackground="#323232", bg="#323232",
+            real_button_360p = Button(root, image=button_360p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                       command=lambda: res_getter("360p"))
             real_button_360p.place(x=190, y=385)
 
             button_480p = PhotoImage(file=resource_path("480p_button.png"))
             button_480p_label = Label(image=button_480p, borderwidth=0)
             button_480p_label.place(x=270, y=385)
-            real_button_480p = Button(root, image=button_480p, borderwidth=0, activebackground="#323232", bg="#323232",
+            real_button_480p = Button(root, image=button_480p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                       command=lambda: res_getter("480p"))
             real_button_480p.place(x=270, y=385)
 
             button_720p = PhotoImage(file=resource_path("720p_button.png"))
             button_720p_label = Label(image=button_720p, borderwidth=0)
             button_720p_label.place(x=350, y=385)
-            real_button_720p = Button(root, image=button_720p, borderwidth=0, activebackground="#323232", bg="#323232",
+            real_button_720p = Button(root, image=button_720p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                       command=lambda: res_getter("720p"))
             real_button_720p.place(x=350, y=385)
 
@@ -124,35 +127,35 @@ def youtube_screen():
             button_144p = PhotoImage(file=resource_path("144p_button.png"))
             button_144p_label = Label(image=button_144p, borderwidth=0)
             button_144p_label.place(x=30, y=385)
-            real_button_144p = Button(root, image=button_144p, borderwidth=0, activebackground="#323232", bg="#323232",
+            real_button_144p = Button(root, image=button_144p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                       command=lambda: res_getter("144p"))
             real_button_144p.place(x=30, y=385)
 
             button_240p = PhotoImage(file=resource_path("240p_button.png"))
             button_240p_label = Label(image=button_240p, borderwidth=0)
             button_240p_label.place(x=110, y=385)
-            real_button_240p = Button(root, image=button_240p, borderwidth=0, activebackground="#323232", bg="#323232",
+            real_button_240p = Button(root, image=button_240p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                       command=lambda: res_getter("240p"))
             real_button_240p.place(x=110, y=385)
 
             button_360p = PhotoImage(file=resource_path("selected_360p_button.png"))
             button_360p_label = Label(image=button_360p, borderwidth=0)
             button_360p_label.place(x=190, y=385)
-            real_button_360p = Button(root, image=button_360p, borderwidth=0, activebackground="#323232", bg="#323232",
+            real_button_360p = Button(root, image=button_360p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                       command=lambda: res_getter("360p"))
             real_button_360p.place(x=190, y=385)
 
             button_480p = PhotoImage(file=resource_path("480p_button.png"))
             button_480p_label = Label(image=button_480p, borderwidth=0)
             button_480p_label.place(x=270, y=385)
-            real_button_480p = Button(root, image=button_480p, borderwidth=0, activebackground="#323232", bg="#323232",
+            real_button_480p = Button(root, image=button_480p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                       command=lambda: res_getter("480p"))
             real_button_480p.place(x=270, y=385)
 
             button_720p = PhotoImage(file=resource_path("720p_button.png"))
             button_720p_label = Label(image=button_720p, borderwidth=0)
             button_720p_label.place(x=350, y=385)
-            real_button_720p = Button(root, image=button_720p, borderwidth=0, activebackground="#323232", bg="#323232",
+            real_button_720p = Button(root, image=button_720p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                       command=lambda: res_getter("720p"))
             real_button_720p.place(x=350, y=385)
 
@@ -160,35 +163,35 @@ def youtube_screen():
             button_144p = PhotoImage(file=resource_path("144p_button.png"))
             button_144p_label = Label(image=button_144p, borderwidth=0)
             button_144p_label.place(x=30, y=385)
-            real_button_144p = Button(root, image=button_144p, borderwidth=0, activebackground="#323232", bg="#323232",
+            real_button_144p = Button(root, image=button_144p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                       command=lambda: res_getter("144p"))
             real_button_144p.place(x=30, y=385)
 
             button_240p = PhotoImage(file=resource_path("240p_button.png"))
             button_240p_label = Label(image=button_240p, borderwidth=0)
             button_240p_label.place(x=110, y=385)
-            real_button_240p = Button(root, image=button_240p, borderwidth=0, activebackground="#323232", bg="#323232",
+            real_button_240p = Button(root, image=button_240p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                       command=lambda: res_getter("240p"))
             real_button_240p.place(x=110, y=385)
 
             button_360p = PhotoImage(file=resource_path("360p_button.png"))
             button_360p_label = Label(image=button_360p, borderwidth=0)
             button_360p_label.place(x=190, y=385)
-            real_button_360p = Button(root, image=button_360p, borderwidth=0, activebackground="#323232", bg="#323232",
+            real_button_360p = Button(root, image=button_360p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                       command=lambda: res_getter("360p"))
             real_button_360p.place(x=190, y=385)
 
             button_480p = PhotoImage(file=resource_path("selected_480p_button.png"))
             button_480p_label = Label(image=button_480p, borderwidth=0)
             button_480p_label.place(x=270, y=385)
-            real_button_480p = Button(root, image=button_480p, borderwidth=0, activebackground="#323232", bg="#323232",
+            real_button_480p = Button(root, image=button_480p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                       command=lambda: res_getter("480p"))
             real_button_480p.place(x=270, y=385)
 
             button_720p = PhotoImage(file=resource_path("720p_button.png"))
             button_720p_label = Label(image=button_720p, borderwidth=0)
             button_720p_label.place(x=350, y=385)
-            real_button_720p = Button(root, image=button_720p, borderwidth=0, activebackground="#323232", bg="#323232",
+            real_button_720p = Button(root, image=button_720p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                       command=lambda: res_getter("720p"))
             real_button_720p.place(x=350, y=385)
 
@@ -196,35 +199,35 @@ def youtube_screen():
             button_144p = PhotoImage(file=resource_path("144p_button.png"))
             button_144p_label = Label(image=button_144p, borderwidth=0)
             button_144p_label.place(x=30, y=385)
-            real_button_144p = Button(root, image=button_144p, borderwidth=0, activebackground="#323232", bg="#323232",
+            real_button_144p = Button(root, image=button_144p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                       command=lambda: res_getter("144p"))
             real_button_144p.place(x=30, y=385)
 
             button_240p = PhotoImage(file=resource_path("240p_button.png"))
             button_240p_label = Label(image=button_240p, borderwidth=0)
             button_240p_label.place(x=110, y=385)
-            real_button_240p = Button(root, image=button_240p, borderwidth=0, activebackground="#323232", bg="#323232",
+            real_button_240p = Button(root, image=button_240p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                       command=lambda: res_getter("240p"))
             real_button_240p.place(x=110, y=385)
 
             button_360p = PhotoImage(file=resource_path("360p_button.png"))
             button_360p_label = Label(image=button_360p, borderwidth=0)
             button_360p_label.place(x=190, y=385)
-            real_button_360p = Button(root, image=button_360p, borderwidth=0, activebackground="#323232", bg="#323232",
+            real_button_360p = Button(root, image=button_360p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                       command=lambda: res_getter("360p"))
             real_button_360p.place(x=190, y=385)
 
             button_480p = PhotoImage(file=resource_path("480p_button.png"))
             button_480p_label = Label(image=button_480p, borderwidth=0)
             button_480p_label.place(x=270, y=385)
-            real_button_480p = Button(root, image=button_480p, borderwidth=0, activebackground="#323232", bg="#323232",
+            real_button_480p = Button(root, image=button_480p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                       command=lambda: res_getter("480p"))
             real_button_480p.place(x=270, y=385)
 
             button_720p = PhotoImage(file=resource_path("selected_720p_button.png"))
             button_720p_label = Label(image=button_720p, borderwidth=0)
             button_720p_label.place(x=350, y=385)
-            real_button_720p = Button(root, image=button_720p, borderwidth=0, activebackground="#323232", bg="#323232",
+            real_button_720p = Button(root, image=button_720p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                       command=lambda: res_getter("720p"))
             real_button_720p.place(x=350, y=385)
 
@@ -275,62 +278,62 @@ def youtube_screen():
         except:
             status_entry.insert(0, "Check your internet connection!")
 
-    link_entry = Entry(root, width=20, borderwidth=0, font=("Kristen ITC", 12), bg="#121212", foreground="white")
+    link_entry = Entry(root, width=20, borderwidth=0, font=default_font, bg="#121212", foreground="white")
     link_entry.place(x=40, y=152, height=30, width=370)
 
-    path_entry = Entry(root, width=20, borderwidth=0, font=("Kristen ITC", 12), bg="#121212", foreground="white")
+    path_entry = Entry(root, width=20, borderwidth=0, font=default_font, bg="#121212", foreground="white")
     path_entry.place(x=40, y=270, height=30, width=370)
 
-    status_entry = Entry(root, width=20, borderwidth=0, font=("Kristen ITC", 17), bg="#000000",
+    status_entry = Entry(root, width=20, borderwidth=0, font=default_font, bg="#121212",
                          foreground="white")
-    status_entry.place(x=57, y=497, height=35, width=325)
+    status_entry.place(x=57, y=550, height=35, width=325)
 
     button_choose_path = PhotoImage(file=resource_path("choose_path_button.png"))
     button_choose_path_label = Label(image=button_choose_path, borderwidth=0)
-    button_choose_path_label.place(x=200, y=315)
-    real_button_choose_path = Button(root, image=button_choose_path, borderwidth=0, activebackground="#323232",
-                                     bg="#323232", command=choose_path)
-    real_button_choose_path.place(x=200, y=315)
+    button_choose_path_label.place(x=200, y=325)
+    real_button_choose_path = Button(root, image=button_choose_path, borderwidth=0, activebackground="#1F1F1F",
+                                     bg="#1F1F1F", command=choose_path)
+    real_button_choose_path.place(x=200, y=325)
 
     button_download = PhotoImage(file=resource_path("download_button.png"))
     button_download_label = Label(image=button_download, borderwidth=0)
-    button_download_label.place(x=200, y=545)
-    real_button_download = Button(root, image=button_download, borderwidth=0, activebackground="#323232", bg="#323232",
+    button_download_label.place(x=200, y=460)
+    real_button_download = Button(root, image=button_download, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                                   command=download)
-    real_button_download.place(x=200, y=545)
+    real_button_download.place(x=200, y=460)
 
     button_144p = PhotoImage(file=resource_path("144p_button.png"))
     button_144p_label = Label(image=button_144p, borderwidth=0)
     button_144p_label.place(x=30, y=385)
-    real_button_144p = Button(root, image=button_144p, borderwidth=0, activebackground="#323232", bg="#323232",
+    real_button_144p = Button(root, image=button_144p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                               command=lambda: res_getter("144p"))
     real_button_144p.place(x=30, y=385)
 
     button_240p = PhotoImage(file=resource_path("240p_button.png"))
     button_240p_label = Label(image=button_240p, borderwidth=0)
     button_240p_label.place(x=110, y=385)
-    real_button_240p = Button(root, image=button_240p, borderwidth=0, activebackground="#323232", bg="#323232",
+    real_button_240p = Button(root, image=button_240p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                               command=lambda: res_getter("240p"))
     real_button_240p.place(x=110, y=385)
 
     button_360p = PhotoImage(file=resource_path("360p_button.png"))
     button_360p_label = Label(image=button_360p, borderwidth=0)
     button_360p_label.place(x=190, y=385)
-    real_button_360p = Button(root, image=button_360p, borderwidth=0, activebackground="#323232", bg="#323232",
+    real_button_360p = Button(root, image=button_360p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                               command=lambda: res_getter("360p"))
     real_button_360p.place(x=190, y=385)
 
     button_480p = PhotoImage(file=resource_path("480p_button.png"))
     button_480p_label = Label(image=button_480p, borderwidth=0)
     button_480p_label.place(x=270, y=385)
-    real_button_480p = Button(root, image=button_480p, borderwidth=0, activebackground="#323232", bg="#323232",
+    real_button_480p = Button(root, image=button_480p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                               command=lambda: res_getter("480p"))
     real_button_480p.place(x=270, y=385)
 
     button_720p = PhotoImage(file=resource_path("720p_button.png"))
     button_720p_label = Label(image=button_720p, borderwidth=0)
     button_720p_label.place(x=350, y=385)
-    real_button_720p = Button(root, image=button_720p, borderwidth=0, activebackground="#323232", bg="#323232",
+    real_button_720p = Button(root, image=button_720p, borderwidth=0, activebackground="#1F1F1F", bg="#1F1F1F",
                               command=lambda: res_getter("720p"))
     real_button_720p.place(x=350, y=385)
 
